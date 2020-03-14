9d31da23616a847246c9fe111c9e7b81eccb6cc5
Iñaki Reta <jirsis@gmail.com>
2020-03-14 15:31:02 +0100

    Definida url base de donde buscar los programas y definido un primer mensaje de logger

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -3,6 +3,13 @@
 import routing
 import logging
 import xbmcaddon
+
+import xbmc
+import urllib2
+import re
+import urllib
+import urlparse
+
 from resources.lib import kodiutils
 from resources.lib import kodilogging
 from xbmcgui import ListItem
@@ -14,13 +21,25 @@ logger = logging.getLogger(ADDON.getAddonInfo('id'))
 kodilogging.config()
 plugin = routing.Plugin()
 
+domain_base = 'https://www.hogarmania.com'
+url_base = domain_base + '/tv/programas/bricomania/'
+
 
 @plugin.route('/')
 def index():
-    addDirectoryItem(plugin.handle, plugin.url_for(
-        show_category, "one"), ListItem("Category One"), True)
-    addDirectoryItem(plugin.handle, plugin.url_for(
-        show_category, "two"), ListItem("Category Two"), True)
+    logger.warn("34567")
+    #http = urllib2.urlopen(url_base)
+    #html = http.read()
+    #http.close()
+
+    #yearsHtml = re.findall('<ul class="anio">(.*?)<ul class=', html, re.DOTALL)[0]
+    #years = re.findall('<li(.*?)</li>', yearsHtml, re.DOTALL)
+    years = ['2019', '2018', '2017', '2016', '2015', '1995']
+    years.reverse()
+
+    for year in years:
+        addDirectoryItem(plugin.handle, plugin.url_for(show_category, year), ListItem("Category "+year), True)
+
     endOfDirectory(plugin.handle)
 
 
